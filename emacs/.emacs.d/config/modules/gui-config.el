40fe5231944ab48af8b2b502a449ebf94f337b5c
ewantown <etown@duck.com>
2024-11-18 17:55:57 -0800

    fixed syntax error

--- emacs/.emacs.d/config/modules/gui-config.el
+++ emacs/.emacs.d/config/modules/gui-config.el
@@ -27,7 +27,7 @@
 	    (vertical-scroll-bars . nil)
 	    (horizontal-scroll-bars . nil)))
     (with-system darwin
-      (add-to-list default-frame-alist (font . "Menlo 13")))
+      (add-to-list 'default-frame-alist '(font . "Menlo 13")))
     (menu-bar-mode -1)
     (tool-bar-mode -1)
     (scroll-bar-mode -1)
