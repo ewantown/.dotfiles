72251e758269f27e97a28710ab4983c67168871e
ewantown <etown@duck.com>
2024-11-20 16:22:08 -0800

    cleaned up org-to-html js

--- emacs/.emacs.d/config/dropins/org-html-themify.js
+++ emacs/.emacs.d/config/dropins/org-html-themify.js
@@ -57,42 +57,33 @@ if (toc) {
 }
 
 function copyTextToClipboard(text) {
-  var textArea = document.createElement("textarea");
-
-  // Place in the top-left corner of screen regardless of scroll position.
+  // Make invisible textarea
+  var textArea = document.createElement("textarea");  
   textArea.style.position = 'fixed';
   textArea.style.top = 0;
   textArea.style.left = 0;
-
-  // Ensure it has a small width and height. Setting to 1px / 1em
-  // doesn't work as this gives a negative w/h on some browsers.
   textArea.style.width = '2em';
   textArea.style.height = '2em';
-
-  // We don't need padding, reducing the size if it does flash render.
   textArea.style.padding = 0;
-
-  // Clean up any borders.
   textArea.style.border = 'none';
   textArea.style.outline = 'none';
   textArea.style.boxShadow = 'none';
-
-  // Avoid flash of the white box if rendered for any reason.
   textArea.style.background = 'transparent';
 
+  // Copy contents into it
   textArea.value = text;
 
+  // Try to copy from it to clipboard
   document.body.appendChild(textArea);
   textArea.focus();
   textArea.select();
-
   try {
-    var successful = document.execCommand('copy');
-    var msg = successful ? 'successful' : 'unsuccessful';
-    console.log('Copying text command was ' + msg);
+    var success = document.execCommand('copy');
+    console.log(success ? 'Copied text' : 'Could not copy text');
   } catch (err) {
-    console.log('Unable to copy text');
+    console.log('Error copying text');
   }
 
+  // Remove invisible textarea
   document.body.removeChild(textArea);
 }
