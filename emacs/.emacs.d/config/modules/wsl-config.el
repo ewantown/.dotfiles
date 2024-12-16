f29690879957778b4ffa29cbb2686cb9e948cac6
ewantown <etown@duck.com>
2024-12-15 20:26:15 -0800

    wsl clipboard and git

--- emacs/.emacs.d/config/modules/wsl-config.el
+++ emacs/.emacs.d/config/modules/wsl-config.el
@@ -5,8 +5,9 @@
 (defun et-init-wsl (conf stem)
   "Initialize on Windows Subsystem for Linux"
   (et-init-wsl-env)
-  (if (not (display-graphic-p))
-      (et-init-wsl-nw-clipboard))    
+  (when (not (display-graphic-p))
+    (et-init-wsl-nw-clipboard)
+    (et-init-git-switcher))
   (use-package sudo-edit
     :bind ("C-c C-r" . 'sudo-edit)))
 ;==============================================================================
@@ -19,6 +20,25 @@
 (defun et-init-wsl-nw-clipboard ()
     (use-package xclip
       :config (xclip-mode 1)))
+
+(defun et-set-magit-executable-for-dir (&optional x &rest _)
+  "Set the appropriate Git executable for the current directory."
+  (if (string-prefix-p "/mnt/" default-directory)
+      (progn
+	(setq magit-git-executable "/mnt/c/Program Files/Git/bin/git.exe")
+	(shell-command "git config --global core.editor \"emacsclient -c -a ''\""))
+    (setq magit-git-executable "/usr/bin/git")))
+
+(defun et-set-git-config-for-magit ()
+  "Set the Git config 'core.editor' to use the Emacs internal editor."
+  (shell-command "git config --global core.editor \"emacsclient -c -a ''\""))
+
+(defun et-init-git-switcher ()  
+  (advice-add 'magit-status :before #'et-set-magit-executable-for-dir)
+  (advice-add 'magit-commit :before #'et-set-magit-executable-for-dir)
+  (advice-add 'magit-push :before #'et-set-magit-executable-for-dir)
+  (advice-add 'magit-pull :before #'et-set-magit-executable-for-dir)
+  (advice-add 'magit-fetch :before #'et-set-magit-executable-for-dir))
 
 (defun git-bash ()
   (interactive)
